e8eb4e62acb03b99e56d2e86fe7ac08cf870a261
Arham Begani <arhambegani2@gmail.com>
2025-12-30 18:18:03 +0530

    fix: wildcard route syntax for Render compatibility

--- server/index.js
+++ server/index.js
@@ -286,7 +286,7 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
